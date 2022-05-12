30872ff1f86c68e93257e891254360c1d5e10943
Pe Pe <pszemek.prabucki@gmail.com>
2022-05-12 20:22:11 +0200

    Add lang definition

---
+++ lang/example.pp
@@ -0,0 +1,51 @@
+void greet() {
+	printString("Hello world");
+
+	return;
+}
+
+int a(int x) {
+	int b(int val) {
+		return 2 * value;
+	}
+
+	return b(x);
+}
+
+int fib(int n) {
+	if (n < 2) {
+		return 1;
+	}
+
+	return fib(n - 1) + fib(n - 2);
+}
+
+void inc(ref int val) {
+	val++;
+
+	return;
+}
+
+void main() {
+	greet();
+
+	int x;
+	x = 2 + 2;
+
+	printBool(x > 3 - 1);
+	printBool(2 * 2 < 3 / 1);
+
+	int i = 0, n = 10, dummy;
+	while (i < n) {
+		printInt(a(i));
+		i++;
+	}
+
+	printInt(fib(5));
+
+	int y = 1;
+	inc(y);
+	printInt(y);
+
+	return;
+}
